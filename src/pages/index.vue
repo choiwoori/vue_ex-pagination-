89cdb7a2206e1cfd0d4aa339955747db7ad4dd7e
woori <woori@maruw.co.kr>
2024-01-02 17:38:22 +0900

    first

---
+++ src/pages/index.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <div class="page">
+        <BasicLayout>
+            <div class="page__container">
+                <section class="page__container__navigation"></section>
+                <section class="page__container__notice"></section>
+            </div>
+        </BasicLayout>
+    </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '@components/atoms/layout/BasicLayout.vue'
+</script>
+
+<style lang="scss" scoped>
+.page{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 100vh;
+
+    background-color: $color-black-900;
+}
+
+</style>
